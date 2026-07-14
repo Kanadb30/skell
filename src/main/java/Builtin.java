87b300d2bc4469058b34d98fcfd3f78d41238162
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-07-14 13:09:38 +0000

    codecrafters submit [skip ci]

--- src/main/java/Builtin.java
+++ src/main/java/Builtin.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-import org.jline.reader.LineReader;
+import org.jline.reader;
 import org.jline.reader.History;
 import org.jline.reader.impl.history.DefaultHistory;
 import org.jline.terminal.*;
