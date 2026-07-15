ce593a34cb9725065a3f4772d25c95d90669e5ea
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-07-15 14:05:05 +0000

    codecrafters submit [skip ci]

--- src/main/java/custom/cmd.java
+++ src/main/java/custom/cmd.java
@@ -3,12 +3,12 @@ package custom;
 import java.util.ArrayList;
 
 public class cmd{
-    String cmd;
-    boolean isBuiltin;
-    char flag;
-    ArrayList<String> Args;
-    boolean hasPath;
-    boolean isPathAbosulte;
+    public String cmd;
+    public boolean isBuiltin;
+    public char flag;
+    public ArrayList<String> Args;
+    public boolean hasPath;
+    public boolean isPathAbosulte;
 
     public cmd(String cmd, boolean isBuiltin, char flag, ArrayList<String> Args, boolean hasPath, boolean isPathAbosulte){
         this.cmd = cmd;
