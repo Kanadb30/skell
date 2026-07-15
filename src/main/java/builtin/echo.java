dadf69284681f7bc0f86cd040637560974067f8c
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-07-15 14:56:06 +0000

    codecrafters submit [skip ci]

--- src/main/java/builtin/echo.java
+++ src/main/java/builtin/echo.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 
 public class echo {
     public static void echo(String[] ARGS) {
-        System.out.println(String.join(" ", Arrays.copyOfRange(ARGS, 1, ARGS.length)));
+        System.out.println(String.join(" ", Arrays.copyOfRange(ARGS, 0, ARGS.length)));
     }
 }
