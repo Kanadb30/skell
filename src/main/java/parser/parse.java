db385b354af76393c87d4204995af7126de609fa
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-07-16 07:50:22 +0000

    codecrafters submit [skip ci]

--- src/main/java/parser/parse.java
+++ src/main/java/parser/parse.java
@@ -102,7 +102,7 @@ public class parse{
                             }
                             itr++;
                         }
-                        if(arg.charAt(i) == "$" && DECLARE_PAIR.getValue(arg.substring(i+1, arg.length())).isEmpty()){
+                        if(arg.charAt(i) == '$' && DECLARE_PAIR.getValue(arg.substring(i+1, arg.length())) == null){
                             arg = arg.substring(0, i);
                             i--;
                         }
