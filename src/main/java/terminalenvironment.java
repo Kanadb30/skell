43c299d8207307bcd2598f7d6852d66b236468e8
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-07-20 11:08:34 +0000

    bell style {trying to add}

--- src/main/java/terminalenvironment.java
+++ src/main/java/terminalenvironment.java
@@ -54,8 +54,8 @@ class terminalEnvironnment {
             .completer(completer)
             .build();
         reader.unsetOpt(LineReader.Option.HISTORY_IGNORE_DUPS);
+        reader.setVariable(LineReader.BELL_STYLE, "visible");
         reader.setOpt(LineReader.Option.AUTO_LIST);   // List all matching choices on double-tab
-        reader.setOpt(LineReader.Option.MENU_COMPLETE); // Cycle through options with the tab key
         reader.unsetOpt(LineReader.Option.INSERT_TAB);  // Disable inserting a tab character on tab key press
         return reader;
     }
